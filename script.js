c95928c40c848922fd3dd29e1c357459f939d941
12amankumar <117712947+12amankumar@users.noreply.github.com>
2023-01-17 12:45:39 +0530

    commit

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your code here
+
+let arr=["img1","img2","img3","img4","img5"];
+let randomindex= Math.floor(Math.random()*arr.length)
+
+arr.push(arr[randomindex])
+console.log("Earlier Array",arr);
+
+let arr1=[]
+
+let randomindex=Math.floor(Math.random()*arr.length)
